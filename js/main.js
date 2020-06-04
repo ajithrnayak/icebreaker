d8663d15ab42442824729eebda26dcbda27f9c5e
Ajith R Nayak <ajithrnayak@icloud.com>
2020-06-04 14:22:34 +0530

    Improved buggy background color generation logic

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-var colors = ["rgba(193, 41, 51, 0.1)", "rgba(241, 211, 2, 0.1)", "rgba(0, 187, 249, 0.1)", "rgba(53, 206, 141, 0.1)", "#CFE0C3", "#F8F7FF", "#FFEEDD", "#FFF8E8"];
-
-function randomNoRepeats(array) {
-  var copy = array.slice(0);
-  return function() {
-    if (copy.length < 1) { copy = array.slice(0); }
-    var index = Math.floor(Math.random() * copy.length);
-    var item = copy[index];
-    copy.splice(index, 1);
-    return item;
-  };
-}
+var colors = [
+  "rgba(193, 41, 51, 0.1)",
+  "rgba(241, 211, 2, 0.1)",
+  "rgba(0, 187, 249, 0.1)",
+  "rgba(53, 206, 141, 0.1)",
+  "#CFE0C3",
+  "#F8F7FF",
+  "#FFEEDD",
+  "#FFF8E8",
+];
 
 $.getJSON("icebreakers.json", function (icebreakers) {
   var questions = icebreakers["questions"];
@@ -17,14 +15,18 @@ $.getJSON("icebreakers.json", function (icebreakers) {
 });
 
 function createIcebreakers(questions) {
-  $.each(questions, function (idx, value) {
-    var random_color = randomNoRepeats(colors);
+  var bgColors = [];
+  for (var i = 0; i < questions.length; i++) {
+    bgColors.push(colors[i % colors.length]);
+  }
+  $.each(questions, function (index, value) {
+    var random_color = bgColors[index];
     var newdiv = $("<div>", {
       class: "questions",
       text: value,
       background: random_color,
     });
-    newdiv.css('background-color', random_color);
+    newdiv.css("background-color", random_color);
     $("main").append(newdiv);
   });
 }
